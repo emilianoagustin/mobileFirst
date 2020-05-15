7321287f230697ad28d7bfb4624cd1d6cb05e0e9
Emiliano Pintos <pintos.emiliano@gmail.com>
2020-05-15 12:23:32 -0300

    Modified estilo.css index.html and main.js

--- main.js
+++ main.js
@@ -1,5 +1,40 @@
 const viewWorkBtn = document.getElementById("#view-work");
 
-viewWorkBtn = document.addEventListener('click', function scrollDown() {
-  console.log(1);
-})
+viewWorkBtn = document.addEventListener('click', scrollDown());
+
+function scrollDown() {
+  window.scrollTo({
+    bottom: 0,
+    behavior: "smooth"
+  })
+}
+
+
+let header = 300;
+
+
+function scrollUp() {
+  if (window.pageYOffset >= header) {
+    document.querySelector('#arrow').classList.add('arrow-fadeIn');
+    document.querySelector('#arrow').classList.remove('arrow-fadeOut');
+  }
+  else{
+    document.querySelector('#arrow').classList.add('arrow-fadeOut');
+    document.querySelector('#arrow').classList.remove('arrow-fadeIn');
+  }
+};
+
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  })
+}
+
+document.getElementById("arrow").addEventListener("click",function(){
+  scrollToTop();
+});
+
+document.addEventListener('scroll', function (){
+  scrollUp();
+});
